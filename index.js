78ea19b77aaad19748702b2deb902d59313dddf0
Anthony Ramirez <tonyjo.ramirez@gmail.com>
2023-04-04 09:22:41 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const bye = document.querySelector('main#main');
+main.remove(bye);
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "Anthony Ramirez is the champion";
+
+document.body.append("newHeader");
